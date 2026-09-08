3ea4e6d8ccb8fd143e9eb015694bb9b614ae96b9
tnsklussenbedrijf-gif <tnsklussenbedrijf@hotmail.com>
2026-09-08 12:03:38 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'luxekozijn-v9';
+const CACHE = 'luxekozijn-v6';
 const ASSETS = [
   './',
   './index.html',
@@ -7,21 +7,14 @@ const ASSETS = [
   './icon-512.png',
   './apple-touch-icon.png'
 ];
-
 self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting())
-  );
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting()));
 });
-
 self.addEventListener('activate', e => {
   e.waitUntil(
-    caches.keys()
-      .then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
-      .then(() => self.clients.claim())
+    caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(() => self.clients.claim())
   );
 });
-
 self.addEventListener('fetch', e => {
   if (e.request.method !== 'GET') return;
   e.respondWith(
